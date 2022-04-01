92d4a74d7ac35fbfd860d8c727790ffc7ab7db50
mustafoa <mustafo2008@gmail.com>
2022-03-31 21:13:22 -0400

    edit files

---
+++ owl/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplayHoverPause: true,
+        mouseDrag: false,
+        slideBy: 1
+    });
+  });
